85f14b1b56965d27f24fb23cc54202d6a921d4b8
Jonathan Herrejón <jonathandaniel.heca@gmail.com>
2017-11-21 02:37:39 -0600

    Se añadió funcionalidad de ocultar teclado.

--- InfStat/ViewController.swift
+++ InfStat/ViewController.swift
@@ -8,17 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
+        var tapGestureRecognizer : UITapGestureRecognizer
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tapGestureRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func dismissKeyboard()
+    {
+        view.endEditing(true)
+    }//end dismissKeyboard
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }//end textFieldShouldReturn
 
 
 }
